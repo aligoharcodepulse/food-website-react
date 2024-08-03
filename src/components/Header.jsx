0caa43cef6440da14c4819cc2ed0c63911f2e84a
Ali Gohar <aligoharkp@gmail.com>
2024-08-03 14:48:14 +0500

    serach container added

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../App.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <input type="text" placeholder="Search Item" />
+      <button>Search</button>
+    </div>
+  )
+}
+
+export default Header
